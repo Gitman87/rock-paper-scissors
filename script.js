5750ef3a2c2db71f82130cc4b9d9f20120871df4
Gitman87 <200~83942039+Gitman87@users.noreply.github.com>
2024-04-15 23:06:12 +0200

    Add messages and score

--- script.js
+++ script.js
@@ -34,36 +34,50 @@ function playRound(playerSelection,computerSelection){
   // playerSelection = playerSelection();
   if(playerSelection == 1 && computerSelection == 2){
     //computer wins
-    console.log("You lose! Paper beats Rock")
+    console.log("You lose! Paper beats Rock");
+    explain.textContent="You lose! Paper beats Rock";
+    container.appendChild(explain);
     return 2;
   }
   else if ( playerSelection ==  1 && computerSelection == 3){
     // player wins
-    console.log("You win! Rock beats Scissors")
+    console.log("You win! Rock beats Scissors");
+    explain.textContent="You win! Rock beats Scissors";
+    container.appendChild(explain);
     return 1;
   }
   else if ( playerSelection == 2 && computerSelection == 1){
     // player wins
-    console.log("You win! Paper beats Rock")
+    console.log("You win! Paper beats Rock");
+    explain.textContent="You win! Paper beats Rock";
+    container.appendChild(explain);
     return 1;
   }
   else if ( playerSelection == 2 && computerSelection == 3){
   //   computer wins
-  console.log("You lose! Scissors beats Paper")
+    console.log("You lose! Scissors beats Paper");
+    explain.textContent="You lose! Scissors beats Paper";
+    container.appendChild(explain);
     return 2;
    }
    else if (playerSelection ==3 && computerSelection == 1){
     // computer wins
-    console.log("You lose! Rock beats Scissors")
+    console.log("You lose! Rock beats Scissors");
+    explain.textContent="You lose! Rock beats Scissors";
+    container.appendChild(explain);
     return 2;
    }
    else if (playerSelection ==3 && computerSelection ==2){
     // player wins
-    console.log("You win! Scissors  beats Paper")
+    console.log("You win! Scissors  beats Paper");
+    explain.textContent="You win! Scissors  beats Paper";
+    container.appendChild(explain);
     return 1;
    }
    else {
-    console.log("It's a tie!")
+    console.log("It's a tie!");
+    explain.textContent="It's a tie!";
+    container.appendChild(explain);
     return 0;
    }
   
@@ -105,49 +119,92 @@ let playerScore=0;
 let compScore=0;
 let numberRounds=0;
 let result=0;
+//selectors
+const container = document.querySelector(".score-container");
+const para = document.createElement("p");
+para.classList.add("para-score");
+para.textContent=`Player: ${playerScore} Comp:${compScore}`;
+container.appendChild(para);
+const explain=document.createElement("p");
+explain.classList.add("para-explain-text");
+explain.textContent="";
+const verdict=document.createElement('p');
+verdict.classList.add('verdict-para');
+verdict.textContent='';
+
+
+
 buttons.forEach((button)=>{
   button.addEventListener("click",()=>{
     let comp=getComputerChoice();
     result = playRound(button.id, comp);
-    if ( result === 1){
+    console.log(button.id, comp);
+    
+    if ( result == 1){
       playerScore++;
       numberRounds++;
     }
-    else if (result === 2){
+    else if (result ==2){
       compScore++;
       numberRounds++;
     }
     else{
+      playerScore++;
+      compScore++;
       numberRounds++;
     }
+    console.log(playerScore, compScore);
+    para.textContent=`Player: ${playerScore} Comp:${compScore}`;
+    container.appendChild(para);
+    
     if (numberRounds>=5){
       if( playerScore > compScore){
-        console.log(`Congrats: You are the winner with score: ${playerScore} to ${playerScore}.`);
+        console.log(`Congrats: You are the winner with score: ${playerScore} to ${compScore}.`);
+        verdict.textContent=`Congrats: You are the winner with score: ${playerScore} to ${compScore}.`
+        container.appendChild(verdict);
         playerScore=0;
         compScore=0;
         numberRounds=0;
         result=0;
+        para.textContent=`Player: ${playerScore} Comp:${compScore}`;
+        container.appendChild(para);
+        explain.textContent="";
+
               }
       else if (playerScore < compScore){
-        console.log(`Congrats: You are the winner with score: ${compScore} to ${playerScore}.`)
+        console.log(`You lose with score: ${playerScore} to ${compScore}.`);
+        verdict.textContent=`You lose with score: ${playerScore} to ${compScore}.`
+        container.appendChild(verdict);
         playerScore=0;
         compScore=0;
         numberRounds=0;
         result=0;
+        para.textContent=`Player: ${playerScore} Comp:${compScore}`;
+        container.appendChild(para);
+        explain.textContent="";
       }
       else {
-        console.log(`It's a tie : ${playerScore} : ${playerScore}`);
+        console.log(`It's a tie : ${playerScore} : ${compScore}`);
+        verdict.textContent=`It's a tie : ${playerScore} : ${compScore}`
+        container.appendChild(verdict);
         playerScore=0;
         compScore=0;
         numberRounds=0;
         result=0;
+        para.textContent=`Player: ${playerScore} Comp:${compScore}`;
+        container.appendChild(para);
+        explain.textContent="";
       }
       
 
     }
+    else{
+      verdict.textContent='';
+    }
+    
     
    
-   console.log(button.id, comp);
+  
 
 
   }
