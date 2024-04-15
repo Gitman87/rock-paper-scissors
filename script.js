49f2203198870337109af291abdbe6b5fb987208
Gitman87 <200~83942039+Gitman87@users.noreply.github.com>
2024-04-15 17:03:27 +0200

    Links external js file

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 
-
 //function for getting computer choice
 function getComputerChoice() {
-  let randomNumber = Math.floor(Math.random()*3);
+  let randomNumber = Math.floor(Math.random()*3) +1;
+  
   return randomNumber;
 }
 //player choice function
 function playerSelection(){
-  let choice = "Type Rock, Paper or Scissors";
-  let choiceCorrected = choice.toLowerCase();
+  let choiceCorrected="";
+  
+  do {
+  let choice = prompt("Type Rock, Paper or Scissors");
+  
+  choiceCorrected = choice.toLowerCase();
+  
+  } while(choiceCorrected !="rock"&& choiceCorrected != "paper" && choiceCorrected !="scissors");
+ 
   switch(choiceCorrected){
     case 'rock':
       return 1;
@@ -25,32 +32,32 @@ function playerSelection(){
 function playRound(playerSelection,computerSelection){
   // computerSelection = getComputerChoice();
   // playerSelection = playerSelection();
-  if(playerSelection === 1 || computerSelection === 2){
+  if(playerSelection === 1 && computerSelection === 2){
     //computer wins
     console.log("You lose! Paper beats Rock")
     return 2;
   }
-  else if ( playerSelection ===  1 || computerSelection === 3){
+  else if ( playerSelection ===  1 && computerSelection === 3){
     // player wins
     console.log("You win! Rock beats Scissors")
     return 1;
   }
-  else if ( playerSelection === 2 || computerSelection === 1){
+  else if ( playerSelection === 2 && computerSelection === 1){
     // player wins
     console.log("You win! Paper beats Rock")
     return 1;
   }
-  else if ( playerSelection === 2 || computerSelection === 3){
+  else if ( playerSelection === 2 && computerSelection === 3){
   //   computer wins
   console.log("You lose! Scissors beats Paper")
     return 2;
    }
-   else if (playerSelection === 3 || computerSelection === 1){
+   else if (playerSelection === 3 && computerSelection === 1){
     // computer wins
     console.log("You lose! Rock beats Scissors")
     return 2;
    }
-   else if (playerSelection === 3 || computerSelection ===2){
+   else if (playerSelection === 3 && computerSelection ===2){
     // player wins
     console.log("You win! Scissors  beats Paper")
     return 1;
@@ -78,10 +85,7 @@ function playGame(){
   else if (result === 2){
     compScore++;
   }
-  else {
-    playerScore++;
-    compScore++;
-  }
+ 
 }
 //compare scores
 if( playerScore > compScore){
@@ -95,5 +99,5 @@ else {
 }
 }
 //execute
-console.log("Hello World");
+console.log("Hello this is Rock Paper Scissors Game. We play 5 rounds. Good luck!");
 playGame();
